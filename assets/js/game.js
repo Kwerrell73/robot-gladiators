b888e6a6e0e71a2c37ad1e7494badd77d7e0946c
Kellie Werrell <kelliewerrell@gmail.com>
2021-03-29 18:16:40 -0700

    added shop input

--- assets/js/game.js
+++ assets/js/game.js
@@ -31,8 +31,8 @@ var fight = function (enemy) {
     while (playerInfo.health > 0 && enemy.health > 0) {
         // ask player if they'd like to fight or skip using fightOrSkip function
         if (fightOrSkip()) {
-          // if true, leave fight by breaking loop
-          break;
+            // if true, leave fight by breaking loop
+            break;
         }
         // generate random damage value based on player's attack power
         var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
@@ -123,22 +123,21 @@ var endGame = function () {
     }
 };
 var shop = function () {
+
     // ask player what they'd like to do
     var shopOptionPrompt = window.prompt(
-        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
-    );
-    // use switch to carry out action
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
+      );
+    shopOptionPrompt = parseInt(shopOptionPrompt);
+    // use switch case to carry out action
     switch (shopOptionPrompt) {
-        case "REFILL":
-        case "refill":
+        case 1:
             playerInfo.refillHealth();
             break;
-        case "UPGRADE":
-        case "upgrade":
+        case 2:
             playerInfo.upgradeAttack();
             break;
-        case "LEAVE": // new case
-        case "leave":
+        case 3:
             window.alert("Leaving the store.");
             break;
         default:
